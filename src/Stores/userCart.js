7daa4bc1f5968f35722cae966f260892e1e950b8
imress_hack <raziel.sier@gmail.com>
2025-01-27 07:12:39 +0300

    main commit

--- src/Stores/userCart.js
+++ src/Stores/userCart.js
@@ -1,14 +1,24 @@
 import { useNotification } from "@kyvg/vue3-notification";
 const { notify }  = useNotification()
 
+const userCart = []
+
+
 export const addToCart = (item) => {
   notify({
     text: "Добавлено в корзину",
   });
 
-  userCart.push(item)
-  console.log(userCart)
+  const itemInCart = userCart.find(cartItem => cartItem.name === item.name)
+
+  if (itemInCart) {
+    itemInCart.count = (itemInCart.count + 1) || 1
+    localStorage.setItem('userCart', JSON.stringify(userCart))
+  } else {
+    item.count = 1
+    userCart.push(item)
+    localStorage.setItem('userCart', JSON.stringify(userCart))
+  }
+
 }
 
-
-const userCart = []
--- src/main.js
+++ src/main.js
@@ -6,9 +6,19 @@ import Notifications from '@kyvg/vue3-notification'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import {addToCart} from "@/Stores/userCart.js";
 
 const app = createApp(App)
 
 app.use(router, Notifications)
 
 app.mount('#app')
+
+
+
+if (localStorage.getItem('userCart')) {
+  for (const item of JSON.parse(localStorage.getItem('userCart'))) {
+    addToCart(item)
+  }
+
+}
